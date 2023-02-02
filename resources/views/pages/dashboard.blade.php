8aa7864340eeafcfae81214d1da0e154e2ef0c08
kidtz <bangkitpr6@gmail.com>
2023-02-02 12:02:04 +0700

    update-navbar-sidebar

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 <!-- -->
 @section('content')
-<div class="section-dashboard px-3">
-    <div class="container-fluid my-4 bg-white border-radius-1rem">
-        <div class="content py-5">
+<div class="section-dashboard">
+    <div class="container-fluid border-radius-1rem">
+        <div class="content py-4">
             <h5>Dashoard</h5>
             <div class="row">
                 <div class="col-lg-9 mb-2">
@@ -11,28 +11,50 @@
                         <div class="col-lg-12 mb-2">
                             <div class="row">
                                 <div class="col-lg-3 mb-2">
-                                    <div class="card py-4">test1</div>
+                                    <div
+                                        class="bg-white box-shadow-grey border-radius-03rem py-4"
+                                    >
+                                        test1
+                                    </div>
                                 </div>
                                 <div class="col-lg-3 mb-2">
-                                    <div class="card py-4">test2</div>
+                                    <div
+                                        class="bg-white box-shadow-grey border-radius-03rem py-4"
+                                    >
+                                        test2
+                                    </div>
                                 </div>
                                 <div class="col-lg-3 mb-2">
-                                    <div class="card py-4">test3</div>
+                                    <div
+                                        class="bg-white box-shadow-grey border-radius-03rem py-4"
+                                    >
+                                        test3
+                                    </div>
                                 </div>
                                 <div class="col-lg-3 mb-2">
-                                    <div class="card py-4">test4</div>
+                                    <div
+                                        class="bg-white box-shadow-grey border-radius-03rem py-4"
+                                    >
+                                        test4
+                                    </div>
                                 </div>
                             </div>
                         </div>
                         <div class="col-lg-12 mb-2">
-                            <div class="card py-5">
+                            <div
+                                class="bg-white box-shadow-grey border-radius-03rem py-5"
+                            >
                                 chart atas bawahnya ntar
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-3 mb-2">
-                    <div class="border border-radius-03rem py-5">side</div>
+                    <div
+                        class="bg-white box-shadow-grey border-radius-03rem py-5"
+                    >
+                        side
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/parts/sidebar.blade.php
+++ resources/views/parts/sidebar.blade.php
@@ -1,81 +1,68 @@
-<div class="navbar-expand-lg bg-white" id="sidebar-desktop">
+<div class="navbar-expand-lg bg-white py-4" id="sidebar-desktop">
     <div class="collapse navbar-collapse">
-        <div class="row flex-nowrap">
-            <div class="col-auto px-sm-2 px-0">
-                <div
-                    class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 min-vh-100"
-                >
-                    <ul
-                        class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
-                        id="menu"
+        <div class="container-fluid px-0">
+            <ul
+                class="navbar-nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
+                id="menu"
+            >
+                <li class="nav-item py-1 w-100">
+                    <a
+                        class="nav-link px-3 w-100"
+                        href="{{ route('dashboard') }}"
                     >
-                        <li class="nav-item">
-                            <a
-                                href="{{ route('dashboard') }}"
-                                class="nav-link align-middle px-0"
-                            >
-                                <i class="fs-4 bi-grid icons-center"></i>
-                                <span class="ms-1 d-none d-sm-inline"
-                                    >Dashboard</span
+                        <i
+                            class="bi bi-grid bi icons-center icons-size-1rem"
+                        ></i
+                        ><span class="d-none d-sm-inline text-top px-2"
+                            >Dashboard</span
+                        >
+                    </a>
+                </li>
+                <li class="nav-item py-1 w-100">
+                    <a class="nav-link px-3 w-100" href="{{ route('buku') }}">
+                        <i
+                            class="bi bi-book bi icons-center icons-size-1rem"
+                        ></i>
+                        <span class="d-none d-sm-inline text-top px-2"
+                            >Data Buku</span
+                        >
+                    </a>
+                </li>
+
+                <li class="nav-item py-1 w-100">
+                    <a
+                        href="#submenu3"
+                        data-bs-toggle="collapse"
+                        class="nav-link px-3 w-100 px-0 align-middle"
+                        role="button"
+                    >
+                        <i class="fs-6 bi-0-circle bi icons-center"></i>
+                        <span class="d-none d-sm-inline text-top px-2"
+                            >Products</span
+                        >
+                    </a>
+                    <ul
+                        class="collapse nav flex-column ms-1"
+                        id="submenu3"
+                        data-bs-parent="#menu"
+                    >
+                        <li class="w-100 nav-link nav-link-child">
+                            <a href="#" class="nav-link px-3 w-100 px-0">
+                                <span class="d-none d-sm-inline text-top px-2"
+                                    >Product</span
                                 >
                             </a>
                         </li>
-                        <li class="nav-item">
-                            <a
-                                href="{{ route('buku') }}"
-                                class="nav-link align-middle px-0"
-                            >
-                                <i class="fs-4 bi-book icons-center"></i>
-                                <span class="ms-1 d-none d-sm-inline"
-                                    >Data Buku</span
+                        <li class="w-100 nav-link nav-link-child">
+                            <a href="#" class="nav-link px-3 w-100 px-0">
+                                <span class="d-none d-sm-inline text-top px-2"
+                                    >Product</span
                                 >
                             </a>
-                        </li>
-                        <li class="nav-item">
-                            <a
-                                href="#submenu3"
-                                data-bs-toggle="collapse"
-                                class="nav-link px-0 align-middle"
-                            >
-                                <i class="fs-4 bi-0-circle icons-center"></i>
-                                <span class="ms-1 d-none d-sm-inline"
-                                    >Products</span
-                                >
-                            </a>
-                            <ul
-                                class="collapse nav flex-column ms-1"
-                                id="submenu3"
-                                data-bs-parent="#menu"
-                            >
-                                <li class="w-100">
-                                    <a href="#" class="nav-link px-0">
-                                        <span class="d-none d-sm-inline"
-                                            >Product</span
-                                        >
-                                        1</a
-                                    >
-                                </li>
-                                <li>
-                                    <a href="#" class="nav-link px-0">
-                                        <span class="d-none d-sm-inline"
-                                            >Product</span
-                                        >
-                                        2</a
-                                    >
-                                </li>
-                                <li>
-                                    <a href="#" class="nav-link px-0">
-                                        <span class="d-none d-sm-inline"
-                                            >Product</span
-                                        >
-                                        3</a
-                                    >
-                                </li>
-                            </ul>
                         </li>
                     </ul>
-                </div>
-            </div>
+                </li>
+            </ul>
         </div>
     </div>
 </div>
